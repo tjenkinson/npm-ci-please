d598a6b6bf5bade02e8b490fb0071d355b8c9959
Tom Jenkinson <tom@tjenkinson.me>
2023-01-16 21:35:41 +0000

    🚀

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # npm-ci-please
-`npm ci` with a fallback to `npm i` when it fails, but shouldn't
+
+`npm ci` with a fallback to `npm i` when it fails, but shouldn't.
+
+To workaround fun errors like:
+
+```
+7:41:18 PM: npm ERR! code EBADPLATFORM
+7:41:18 PM: npm ERR! notsup Unsupported platform for @esbuild/android-arm@0.15.15: wanted {"os":"android","arch":"arm"} (current: {"os":"linux","arch":"x64"})
+7:41:18 PM: npm ERR! notsup Valid OS:    android
+7:41:18 PM: npm ERR! notsup Valid Arch:  arm
+7:41:18 PM: npm ERR! notsup Actual OS:   linux
+7:41:18 PM: npm ERR! notsup Actual Arch: x64
+7:41:18 PM: npm ERR! A complete log of this run can be found in:
+7:41:18 PM: npm ERR!     /opt/buildhome/.npm/_logs/2022-12-18T19_41_17_238Z-debug-0.log
+```
+
+See [this issue](https://github.com/netlify/cli/issues/5323) for one case where `npm ci` is broken and the above error happens.
+
+Ideally npm will fix this bug and this workaround will not be needed.
+
+## How?
+
+This tool will first run `npm ci`, and if that doesn't work it will:
+
+- take a copy of the current "package-lock.json"
+- run `npm install`
+- check if "package-lock.json" contents has changed, and if it has exit with 1, otherwise exit with 0
+
+Note this fallback is not intended to mimick exactly what `npm ci` does. E.g. with `npm ci` it won't even try to install anything or run package scripts if the "package-lock.json" is not in sync.
+
+## Usage
+
+```sh
+npx npm-ci-please@1
+```
+
+If you want to skip even trying the native `npm ci` and go straight to the workaround add the `--skip-native` flag.
---
+++ src/index.js
@@ -0,0 +1,104 @@
+const { readFileSync, writeFileSync } = require('node:fs');
+const { spawn } = require('node:child_process');
+
+const skipNative = process.argv.slice(2).includes(`--skip-native`);
+
+const onSigInt = new Set();
+
+const log = (msg) => console.log(`[npm-ci-please] ${msg}`);
+
+const invalidPackageLock = Symbol('invalidPackageLock');
+const exception = Symbol('exception');
+const success = Symbol('success');
+
+const exec = (command, ...args) => {
+  return new Promise((resolve, reject) => {
+    const handle = spawn(command, args, {
+      stdio: ['ignore', 'inherit', 'inherit'],
+    });
+
+    const kill = () => handle.kill();
+    onSigInt.add(kill);
+
+    handle.once('close', (code) => {
+      onSigInt.delete(kill);
+      if (code === 0) {
+        resolve();
+      } else {
+        reject(new Error(`Exit code: ${code}`));
+      }
+    });
+  });
+};
+
+const readPackageLock = () => {
+  return readFileSync(`./package-lock.json`, {
+    encoding: 'utf8',
+  });
+};
+
+const tryNative = async () => {
+  try {
+    await exec(`npm`, `ci`);
+  } catch (e) {
+    return exception;
+  }
+
+  return success;
+};
+
+const tryWorkaround = async () => {
+  const packageLock = readPackageLock();
+
+  try {
+    await exec(`npm`, `install`);
+  } catch {
+    return exception;
+  }
+
+  if (readPackageLock() !== packageLock) {
+    writeFileSync(`./package-lock.json`, packageLock);
+    return invalidPackageLock;
+  }
+
+  return success;
+};
+
+const go = async () => {
+  process.on('SIGINT', () => {
+    onSigInt.forEach((cb) => cb());
+    console.log('Aborting...');
+    process.exit(1);
+  });
+
+  if (!skipNative) {
+    log('Trying native `npm ci`');
+    if ((await tryNative()) === success) {
+      log('Native `npm ci` worked');
+      process.exit(0);
+    }
+
+    log('Native `npm ci` failed');
+  }
+
+  log('Performing workaround...');
+
+  const workaroundRes = await tryWorkaround();
+  if (workaroundRes === success) {
+    log('Workaround succeeded');
+    process.exit(0);
+  }
+  if (workaroundRes === invalidPackageLock) {
+    log(`Failed: package-lock.json changed and is therefore not in sync`);
+  } else {
+    log(`Failed: unexpected error`);
+  }
+
+  process.exit(1);
+};
+
+go().catch((e) => {
+  console.error(`Unexpected error`);
+  console.error(e);
+  process.exit(1);
+});
